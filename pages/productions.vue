8af1789b71ff2f413d0131a3dc8b08269b2ee952
Isaac Chabon <ichabon@me.com>
2025-01-19 20:16:35 -0500

    add productions!

--- pages/productions.vue
+++ pages/productions.vue
@@ -11,16 +11,34 @@
 
 
     </Main>
-
     <Main v-if="activeIndex !== null" class="productions-media-window">
-      <div class="production-media" v-for="(production, index) in doc.productions">
-        <div class="production-media-content" v-if="activeIndex === index">
+      <div v-for="(production, index) in doc.productions">
+        <div v-if="activeIndex === index">
           <div class="topbar">
             <button class="close-btn" @click="() => setActiveIndex(null)">
               <NuxtImg src="/images/close.svg"/>
             </button>
             <p class="title">{{ production.title }}</p>
+
           </div>
+          <div class="window-body">
+            <p v-if="production.description" v-html="production.description"/>
+            <NuxtLink v-if="production.link" :to="production.link.src" >{{production.link.title}}</NuxtLink>
+
+            <div class="media-grid" v-if="production.media">
+              <div class="media-item" v-for="media in production.media" >
+                <NuxtImg :src="media.src" v-if="media.type==='image'"/>
+              </div>
+            </div>
+
+            <ul class="links" v-if="production.links">
+              <li v-for="link in production.links">
+                <NuxtLink :to="link.src">{{ link.title }}</NuxtLink>
+              </li>
+            </ul>
+
+          </div>
+
         </div>
       </div>
     </Main>
@@ -91,6 +109,8 @@
     padding: 16px 0;
     width: 100%;
     position: relative;
+    position: sticky;
+    top: 0;
   }
 
   .title {
@@ -120,6 +140,29 @@
       width: 32px;
       height: 32px;
       top: 16px;
+    }
+  }
+
+  .window-body {
+    padding: 32px 24px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    @media (min-width: 768px) {
+      padding: 48px 40px;
+    }
+
+    a {
+      text-align: center;
+    }
+  }
+  .media-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    flex-direction: column;
+    gap: 8px;
+    img {
+      width: 100%;
     }
   }
 
